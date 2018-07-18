a07e2034dfd170a1882a89506c0452923a1ba270
braderbenjun <38066572+braderbenjun@users.noreply.github.com>
2018-07-18 14:18:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	newname = name + " likes to throw shurikens"
-	return render_template('index.html', name=name)
+	new_name = name + " likes to throw shurikens"
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
